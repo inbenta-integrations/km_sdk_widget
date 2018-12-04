8b717437afe95204aa728424894d5393c8b5b4d2
mgarriga <mgarriga@inbenta.com>
2018-12-04 18:19:54 +0100

    Maintain session

--- src/js/sdk-widget.js
+++ src/js/sdk-widget.js
@@ -49,12 +49,54 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
         addBanner();
         addWidget();
         addEventListener();
+        setInitialStatus();
     }
+
+    function setInitialStatus(){
+        var isOpen = getWidgetStorageItem('isOpen');
+        if(isOpen){
+            openWidget();
+        }
+    }
+
     function initSDK(){
         sdk = InbentaKmSDK.createFromDomainKey(domainKey,key,options);
         initSDKComponents();
+        setWidgetStatus();
         linkSDKComponents();
         eventsSDKComponents();
+    }
+
+    function setWidgetStatus(){
+        var page = getWidgetStorageItem('page');
+        if(page){
+            switch (page) {
+                case 'home':
+                    goHome();
+                    break;
+                case 'results':
+                    goResults();
+                    break;
+                case 'result':
+                    goResult();
+                    break;
+                case 'categories':
+                    goCategories();
+                    break;
+            }
+        }
+        var storedCategory = getWidgetStorageItem('category');
+        if(storedCategory){
+            categories.setCategory(storedCategory);
+        }
+        var storedContent = getWidgetStorageItem('content');
+        if(storedContent){
+            result.openContentById(storedContent);
+        }
+        var storedQuery = getWidgetStorageItem('query');
+        if(storedQuery){
+            results.setQuery(storedQuery);
+        }
     }
 
     function initSDKComponents(){
@@ -79,50 +121,66 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
     }
 
     function eventsSDKComponents(){
-        categories.$on( 'click', () => {
+        categories.$on( 'click', (category) => {
+            setWidgetStorageItem('category', category.id);
             goCategories();
         });
-        
+
         categories.$on( 'contentClick', (content) => {
+            setWidgetStorageItem('content', content.id);
             goResult('categories');
             result.openContentById(content.id, false);
         });
 
-        searchBox.$on( 'query', () => {
+        searchBox.$on( 'query', (query) => {
+            setWidgetStorageItem('query', query);
             goResults();
         });
 
         results.$on( 'click', (content) => {
+            setWidgetStorageItem('content', content.id);
             goResult('results');
             result.openContentById(content.id, false);
         });
 
         popular.$on( 'click', (content) => {
+            setWidgetStorageItem('content', content.id);
             goResult('home');
             result.openContentById(content.id, false);
         });
 
         autocompleter.$on( 'autocompleterClick', (content) => {
+            setWidgetStorageItem('content', content.id);
             goResult('home');
         });
     }
 
+    function openWidget(){
+        banner.classList.add("inbenta-km--hidden");
+        widget.classList.remove("inbenta-km--hidden");
+        if(sdk==null){
+            initSDK();
+        }
+    }
+
     function addEventListener(){
         banner.onclick = function(e){
-            banner.classList.add("inbenta-km--hidden");
-            widget.classList.remove("inbenta-km--hidden");
-            if(sdk==null){
-                initSDK();
-            }
+            openWidget();
+            setWidgetStorageItem('isOpen', true);
         }
 
         widgetMinimize.onclick = function(e){
             banner.classList.remove("inbenta-km--hidden");
             widget.classList.add("inbenta-km--hidden");
+            setWidgetStorageItem('isOpen', false);
         }
 
         widgetClose.onclick = function(e){
+            goHome();
+            removeWidgetStorage();
+            banner.classList.remove("inbenta-km--hidden");
             widget.classList.add("inbenta-km--hidden");
+            setWidgetStorageItem('isOpen', false);
         }
 
         widgetBack.onclick = function(e){
@@ -162,7 +220,7 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
                 widgetBack = document.createElement("div");
                 widgetHeader.appendChild(widgetBack);
                 widgetBack.className = 'header__actions__icon inbenta-km-icon inbenta-km-icon--arrow-filled-left inbenta-km--hide';
-                
+
                 var widgetTitle = document.createElement("div");
                 widgetHeader.appendChild(widgetTitle);
                 widgetTitle.className = 'header__title';
@@ -185,11 +243,8 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
             widgetBody = document.createElement("div");
             widget.appendChild(widgetBody);
             widgetBody.className = 'inbenta-km-widget-body';
-
-                //var widgetBody = document.createElement("div");
-                //widgetBody.appendChild(widgetBody);
-                widgetBody.className = 'inbenta-km-widget-body';
-                widgetBody.innerHTML = "<div id='search-box'></div><div id='autocompleter'>";
+            widgetBody.className = 'inbenta-km-widget-body';
+            widgetBody.innerHTML = "<div id='search-box'></div><div id='autocompleter'>";
 
                 sdkHome = document.createElement("div");
                 widgetBody.appendChild(sdkHome);
@@ -210,14 +265,15 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
                 widgetBody.appendChild(sdkResult);
                 sdkResult.className = 'inbenta-km-result inbenta-km--hidden';
                 sdkResult.innerHTML = "</div><div id='result'></div>";
-            
+
             var widgetPowered = document.createElement("div");
             widget.appendChild(widgetPowered);
-            widgetPowered.className = 'inbenta-km-widget-powered';     
+            widgetPowered.className = 'inbenta-km-widget-powered';
             widgetPowered.innerHTML = "<img src='https://cdn.inbenta.io/static/images/logo-inbenta.png'>";
     }
 
     function goHome(){
+        setWidgetStorageItem('page', 'home');
         sdkHome.classList.remove("inbenta-km--hidden");
         sdkCategories.classList.remove("inbenta-km--hidden");
         sdkResult.classList.add("inbenta-km--hidden");
@@ -228,6 +284,7 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
     }
 
     function goResults(){
+        setWidgetStorageItem('page', 'results');
         back = 'home';
         sdkHome.classList.add("inbenta-km--hidden");
         sdkCategories.classList.add("inbenta-km--hidden");
@@ -237,6 +294,7 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
     }
 
     function goResult(backR = 'home'){
+        setWidgetStorageItem('page', 'result');
         back = backR;
         sdkHome.classList.add("inbenta-km--hidden");
         sdkCategories.classList.add("inbenta-km--hidden");
@@ -246,11 +304,31 @@ function createInbentaKmSDKWidget(version, domainKey, key, options){
     }
 
     function goCategories(){
+        setWidgetStorageItem('page', 'categories');
         back = 'home';
         sdkCategories.classList.remove("inbenta-km--hidden");
         widgetBack.classList.remove("inbenta-km--hide");
         sdkHome.classList.add("inbenta-km--hidden");
         sdkResult.classList.add("inbenta-km--hidden");
         sdkResults.classList.add("inbenta-km--hidden");
+    }
+
+    function getWidgetStorageItem(storageKey){
+        let storage = localStorage.getItem(`inbenta_km_widget_storage_${key}`);
+        storage = storage ? storage = JSON.parse(storage) : {};
+        const result = storage[storageKey] ? storage[storageKey] : false;
+
+        return result;
+    }
+
+    function setWidgetStorageItem(storageKey, value){
+        let storage = localStorage.getItem(`inbenta_km_widget_storage_${key}`);
+        storage = storage ? storage = JSON.parse(storage) : {};
+        storage[storageKey] = value;
+        localStorage.setItem(`inbenta_km_widget_storage_${key}`, JSON.stringify(storage));
+    }
+
+    function removeWidgetStorage(){
+        localStorage.removeItem(`inbenta_km_widget_storage_${key}`);
     }
 }
